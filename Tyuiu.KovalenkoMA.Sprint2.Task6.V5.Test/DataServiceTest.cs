a5d72eb3922892b799a011337dc4a1189087a744
Margo Kovalenko <Tyuiu.KovalenkoM@outlook.com>
2025-10-14 23:52:07 +0500

    Task6

---
+++ Tyuiu.KovalenkoMA.Sprint2.Task6.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+using Tyuiu.KovalenkoMA.Sprint2.Task6.V5.Lib;
+namespace Tyuiu.KovalenkoMA.Sprint2.Task6.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res1 = ds.FindCardValue(6);
+            var res2 = ds.FindCardValue(14);
+            Assert.AreEqual("шестерка", res1);
+            Assert.AreEqual("туз", res2);
+        }
+    }
+}
---
+++ Tyuiu.KovalenkoMA.Sprint2.Task6.V5/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.KovalenkoMA.Sprint2.Task6.V5.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнила: Коваленко М. А. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Оператор switch                                                   *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #5                                                              *");
+        Console.WriteLine("* Выполнила:  Коваленко Маргарита Александровна | ПИНб-25-1               *");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая использует оператор switch, вычисляет       *");
+        Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.Write("Введите номер карты: ");
+        int k = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine($"Наименование карты: {ds.FindCardValue(k)}");
+        Console.ReadKey();
+    }
+}
